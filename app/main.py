78822b7956dda47afcbe75fbcbbb9ae5af6784dc
pan-ck <viviwallwall@gmail.com>
2026-09-04 00:28:02 +0800

    successfully loaded API key

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 import csv
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+# setting paths
 ROOT = Path(__file__).resolve().parent.parent
 IOC_CSV = ROOT / 'ioc.csv' # the path to ioc.csv
 ABUSE_DB_TXT = ROOT / 'abuse_db.txt' # the path to abuse_db.txt
 print("IOC_CSV: \n",(IOC_CSV))
 print("ABUSE_DB_TXT: \n", ABUSE_DB_TXT)
 
-valid_IPs = [] # a list to store valid IP
+# loading the .env file
+load_dotenv(ROOT / ".env")
+
+# loading the API key from the .env file
+AbuseIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
+# checking if the API key is loaded
+if not AbuseIPDB_API_KEY:
+    raise ValueError("ABUSEIPDB_API_KEY is missing; save the project .env file")
+print("AbuseIPDB_API_KEY loaded")
 
 # function to get the ioc list from the csv file
 def get_ioc_dict() -> list:
@@ -21,7 +32,7 @@ def get_ioc_dict() -> list:
         print(f"Error when opening the file: {e}")
 
 # function to get the abuse db list from the txt file
-def get_abuse_db_list() -> list:
+def get_test_ip_list() -> list:
     try: #try to open the file
         with open(ABUSE_DB_TXT, 'r', encoding='utf-8') as file:
             return_list = []
@@ -35,6 +46,7 @@ def get_abuse_db_list() -> list:
 # a list including ioc dictionaries
 ioc_list = get_ioc_dict()
 # a list including abuse db ips
-abuse_db_list = get_abuse_db_list()
-print(f"abuse db list: \n{abuse_db_list}")
+test_ip_list = get_test_ip_list()
+
+print(f"abuse db list: \n{test_ip_list}")
 print(f"ioc list: \n{ioc_list}")
