63ff78b913597a0b50a95fb979f9c091ebcf3df0
pan-ck <viviwallwall@gmail.com>
2026-09-04 15:17:50 +0800

    successfully fetched data from AbuseIPDB API. Also extracted the result we want as dict{} format

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ import csv
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import json
+import requests
 
 # setting paths
 ROOT = Path(__file__).resolve().parent.parent
 IOC_CSV = ROOT / 'ioc.csv' # the path to ioc.csv
 ABUSE_DB_TXT = ROOT / 'abuse_db.txt' # the path to abuse_db.txt
-print("IOC_CSV: \n",(IOC_CSV))
-print("ABUSE_DB_TXT: \n", ABUSE_DB_TXT)
 
 # loading the .env file
 load_dotenv(ROOT / ".env")
@@ -19,6 +19,38 @@ AbuseIPDB_API_KEY = os.getenv("ABUSEIPDB_API_KEY")
 if not AbuseIPDB_API_KEY:
     raise ValueError("ABUSEIPDB_API_KEY is missing; save the project .env file")
 print("AbuseIPDB_API_KEY loaded")
+
+# API endpoint
+ABUSEIPDB_API_ENDPOINT = "https://api.abuseipdb.com/api/v2/check"
+
+# a function to extract the data we need
+def extract_API_data(response: dict) -> dict:
+    data = response['data']
+    return {
+        'ipAddress': data['ipAddress'],
+        'abuseConfidenceScore': data['abuseConfidenceScore'],
+        'countryCode': data['countryCode'],
+    }
+
+# function to check the list of ips against the AbuseIPDB API
+def check_ips_abuseipdb(ips: list, api_key: str) -> list:
+    headers = {
+        'Accept': 'application/json',
+        'Key': api_key
+    }
+    results = [] # a list to store the results we want
+    # check IP one by one
+    try:
+        for ip in ips:
+            query_string = {
+                'ipAddress': ip,
+            }
+            response = requests.get(ABUSEIPDB_API_ENDPOINT, headers=headers, params=query_string)
+            response = response.json() # convert json to dict
+            results.append(extract_API_data(response))
+    except Exception as e:
+        print(f"Error when checking the ips with AbuseIPDB API: {e}")
+    return results
 
 # function to get the ioc list from the csv file
 def get_ioc_dict() -> list:
@@ -48,5 +80,8 @@ ioc_list = get_ioc_dict()
 # a list including abuse db ips
 test_ip_list = get_test_ip_list()
 
-print(f"abuse db list: \n{test_ip_list}")
-print(f"ioc list: \n{ioc_list}")
+print(f"test ip list: \n{json.dumps(test_ip_list, indent=4)}")
+print(f"ioc list: \n{json.dumps(ioc_list, indent=4)}")
+
+abuseipdb_results = check_ips_abuseipdb(test_ip_list, AbuseIPDB_API_KEY)
+print(f"abuseipdb results: \n{json.dumps(abuseipdb_results, indent=4)}")
